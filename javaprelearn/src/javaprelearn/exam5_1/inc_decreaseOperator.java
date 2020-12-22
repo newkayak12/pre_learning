7e9d16314e3df9fbf15f11c0b6b1ed4d9ab6fe00
newkayak12 <newkayak12@gmail.com>
2020-12-22 19:54:14 +0900

    prelearn

---
+++ javaprelearn/src/javaprelearn/exam5_1/inc_decreaseOperator.java
@@ -0,0 +1,17 @@
+package javaprelearn.exam5_1;
+
+public class inc_decreaseOperator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int iNum1=10, result;
+result = ++iNum1;//연산 시작 전에 ++
+System.out.println(result);
+System.out.println(iNum1);
+result = iNum1++;// 연산이 다 끝난 후에 ++
+System.out.println(result);
+System.out.println(iNum1);
+
+	}
+
+}
---
+++ javaprelearn/src/javaprelearn/exam5_2/substituteOperator.java
@@ -0,0 +1,12 @@
+package javaprelearn.exam5_2;
+
+public class substituteOperator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	int result =10;
+	result += 1;//result = result +1;
+	System.out.println(result);
+	}
+
+}
