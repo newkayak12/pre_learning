1ca7e92a6167c297dfd32003648f597e23cb0ef4
newkayak12 <newkayak12@gmail.com>
2020-12-23 01:18:09 +0900

    prelearn

---
+++ javaprelearn/src/javaprelearn/exam8_1/singleArray.java
@@ -0,0 +1,12 @@
+package javaprelearn.exam8_1;
+
+public class singleArray {
+public static void main(String[] args) {
+int data[]=new int[5];
+for (int i =0; i<5; i++) {
+	data[i] = i;
+System.out.print("|" + data[i]);	
+}
+
+	}
+}
